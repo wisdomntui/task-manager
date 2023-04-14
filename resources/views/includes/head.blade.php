33a104153fee481a2bdaaebd83c44deed30d457c
Wisdom Ntui <wisdomntui@gmail.com>
2023-04-14 13:13:41 +0100

    Add layout structure

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+
+<title>Task Manager</title>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+   
+<!-- Styles -->
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
